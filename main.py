6e4996dcdedd735a30b7ac02628446c4dd9aaf9f
Bono Nonchev <11063463+madcowbg@users.noreply.github.com>
2025-02-28 13:42:21 +0200

    added resolving of remote names

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import uuid
 from datetime import datetime
-from typing import Generator, List, Tuple
+from typing import Generator, List, Tuple, Dict
 
 import fire
 import logging
@@ -130,15 +130,28 @@ class RepoCommand:
 
         logging.info(f"Refresh done!")
 
+    def _remotes_names(self) -> Dict[str, str]:
+        logging.info(f"Reading config...")
+        config = self._config()
+        return dict((props["name"], remote) for remote, props in config["remotes"].items() if "name" in props)
+
     def show(self, remote: str = "current"):
+        remote_uuid = self._resolve_remote_uuid(remote)
+
         logging.info(f"Reading repo {self.repo}...")
-        doc = read_contents_toml(self.repo, remote=remote)
+        doc = read_contents_toml(self.repo, remote=remote_uuid)
         logging.info(f"Read repo!")
 
-        print(f"Updated on {datetime.fromisoformat(doc["config"]["updated"])}.")
+        print(f"Result for [{remote}] with UUID {remote_uuid}.")
+        print(f"Last updated on {datetime.fromisoformat(doc["config"]["updated"])}.")
         print(f"  # files = {len([f for f, data in doc['fsobjects'].items() if not data['isdir']])}"
               f" of size {format_size(sum(data['size'] for f, data in doc['fsobjects'].items() if not data['isdir']))}")
         print(f"  # dirs  = {len([f for f, data in doc['fsobjects'].items() if data['isdir']])}")
+
+    def _resolve_remote_uuid(self, remote):
+        remotes = self._remotes_names()
+        remote_uuid = remotes[remote] if remote in remotes else remote
+        return remote_uuid
 
     # def status(self):
     #     if not os.path.isfile(os.path.join(hoard_folder(self.repo), "current.contents")):
@@ -154,8 +167,10 @@ class RepoCommand:
 
         remotes_doc = config_doc["remotes"]
         print(f"{len(remotes_doc)} total remotes.")
-        for remote, props in remotes_doc.items():
-            print(f"  {remote}")
+        for remote_uuid, props in remotes_doc.items():
+            name_prefix = f"[{props['name']}] " if "name" in props else ""
+
+            print(f"  {name_prefix}{remote_uuid}")
 
     def _config(self):
         config_file = os.path.join(hoard_folder(self.repo), CONFIG_FILE)
@@ -163,20 +178,27 @@ class RepoCommand:
         if not os.path.isfile(config_file):
             current_uuid = load_uuid(self.repo)
             with open(config_file, "w", encoding="utf-8") as f:
-                rtoml.dump({"remotes": {current_uuid: {"local_path": self.repo}}}, f)
+                rtoml.dump({
+                    "paths": {
+                        current_uuid: self.repo
+                    },
+                    "remotes": {
+                        current_uuid: {"name": "local-repo"}
+                    }}, f)
 
         with open(config_file, "r", encoding="utf-8") as f:
             return rtoml.load(f)
 
     def config_remote(self, remote: str, param: str, value: str):
+        remote_uuid = self._resolve_remote_uuid(remote)
         logging.info(f"Reading config in {self.repo}...")
         config_doc = self._config()
 
-        if remote not in config_doc["remotes"]:
-            raise ValueError(f"remote {remote} does not exist")
+        if remote_uuid not in config_doc["remotes"]:
+            raise ValueError(f"remote_uuid {remote_uuid} does not exist")
 
         logging.info(f"Setting {param} to {value}")
-        config_doc["remotes"][remote][param] = value
+        config_doc["remotes"][remote_uuid][param] = value
 
         logging.info(f"Writing config in {self.repo}...")
         with open(os.path.join(hoard_folder(self.repo), CONFIG_FILE), "w", encoding="utf-8") as f:
