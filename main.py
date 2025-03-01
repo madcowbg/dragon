517bd3b87079329e6158f33097dac247ad5b6e07
Bono Nonchev <11063463+madcowbg@users.noreply.github.com>
2025-03-01 17:40:32 +0200

    added strong typing for TOML manipulation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import shutil
 import uuid
 from datetime import datetime
 from typing import Generator, List, Tuple, Dict, Any
@@ -9,8 +8,6 @@ import logging
 import rtoml
 from alive_progress import alive_bar
 
-from util import format_size
-
 CONFIG_FILE = "hoard.config"
 CURRENT_UUID_FILENAME = "current.uuid"
 NONE_TOML = "MISSING"
@@ -18,6 +15,7 @@ HOARD_CONTENTS_FILENAME = "hoard.contents"
 
 
 def validate_repo(repo: str):
+    logging.info(f"Validating {repo}")
     if not os.path.isdir(repo):
         raise ValueError(f"folder {repo} does not exist")
     if not os.path.isdir(hoard_folder(repo)):
@@ -48,9 +46,145 @@ def walk_repo(repo: str) -> Generator[Tuple[str, List[str], List[str]], None, No
         yield dirpath, dirnames, filenames
 
 
-def read_contents_toml(repo: str, remote: str):
-    with open(os.path.join(hoard_folder(repo), f"{remote}.contents"), "r", encoding="utf-8") as f:
-        return rtoml.load(f)
+class Config:
+    def __init__(self, config_doc: Dict[str, Any]):
+        self.doc = config_doc
+
+    def touch_updated(self):
+        self.doc["updated"] = datetime.now().isoformat()
+
+    @property
+    def updated(self):
+        return datetime.fromisoformat(self.doc["updated"])
+
+
+class FileProps:
+    def __init__(self, doc: Dict[str, Any]):
+        self.doc = doc
+
+    @property
+    def size(self):
+        return self.doc["size"]
+
+    @property
+    def mtime(self):
+        return self.doc["mtime"]
+
+
+class DirProps:
+    def __init__(self, doc: Dict[str, Any]):
+        self.doc = doc
+
+
+class FSObjects:
+    def __init__(self, fsobjects_doc: Dict[str, Any]):
+        self.doc = fsobjects_doc
+        self.files = dict((f, FileProps(data)) for f, data in self.doc.items() if not data['isdir'])
+        self.dirs = dict((f, DirProps(data)) for f, data in self.doc.items() if data['isdir'])
+
+    def add_file(self, fullpath: str, size: int, mtime: float):
+        self.doc[fullpath] = {"size": size, "mtime": mtime, "isdir": False}
+        self.files[fullpath] = FileProps(self.doc[fullpath])
+
+    def add_dir(self, fullpath):
+        self.doc[fullpath] = {"isdir": True}
+        self.dirs[fullpath] = DirProps(self.doc[fullpath])
+
+
+class Contents:
+    @staticmethod
+    def load(filepath: str):
+        with open(filepath, "r", encoding="utf-8") as f:
+            return Contents(filepath, rtoml.load(f))
+
+    def __init__(self, filepath: str, contents_doc: Dict[str, Any]):
+        self.filepath = filepath
+        self.config = Config(contents_doc["config"] if "config" in contents_doc else {})
+        self.fsobjects = FSObjects(contents_doc["fsobjects"] if "fsobjects" in contents_doc else {})
+
+    def write(self):
+        with open(self.filepath, "w", encoding="utf-8") as f:
+            rtoml.dump({
+                "config": self.config.doc,
+                "fsobjects": self.fsobjects.doc
+            }, f)
+
+
+class HoardFileProps:
+    def __init__(self, doc: Dict[str, Any]):
+        self.doc = doc
+
+    @property
+    def size(self):
+        return self.doc["size"]
+
+    @property
+    def mtime(self):
+        return self.doc["mtime"]
+
+    def update(self, props: FileProps):
+        self.doc["size"] = props.size
+        self.doc["mtime"] = props.mtime
+
+
+class HoardFSObjects:
+    def __init__(self, doc: Dict[str, Any]):
+        self.doc = doc
+        self.files = dict((f, HoardFileProps(data)) for f, data in self.doc.items() if not data['isdir'])
+        self.dirs = dict((f, DirProps(data)) for f, data in self.doc.items() if data['isdir'])
+
+    def add_available_file(self, curr_file: str, props: FileProps, current_uuid: str):
+        self.doc[curr_file] = {
+            "isdir": False,
+            "size": props.size,
+            "mtime": props.mtime,
+            "available": current_uuid
+        }
+
+        self.files[curr_file] = HoardFileProps(self.doc[curr_file])
+
+    def add_dir(self, curr_dir: str):
+        self.doc[curr_dir] = {"isdir": True}
+        self.dirs[curr_dir] = DirProps(self.doc[curr_dir])
+
+    def update_file(self, curr_file: str, props: FileProps):
+        self.files[curr_file].update(props)
+
+
+class HoardContents:
+    @staticmethod
+    def load(filename: str) -> "HoardContents":
+        if not os.path.isfile(filename):
+            with open(filename, "w", encoding="utf-8") as f:
+                config = {"updated": datetime.now().isoformat()}
+                rtoml.dump({
+                    "config": config,
+                    "fsobjects": {},
+                }, f)
+        with open(filename, "r", encoding="utf-8") as f:
+            return HoardContents(filename, rtoml.load(f))
+
+    def __init__(self, filepath: str, contents_doc: Dict[str, Any]):
+        self.filepath = filepath
+        self.config = Config(contents_doc["config"] if "config" in contents_doc else {})
+        self.fsobjects = HoardFSObjects(contents_doc["fsobjects"] if "fsobjects" in contents_doc else {})
+
+    def write(self):
+        with open(self.filepath, "w", encoding="utf-8") as f:
+            rtoml.dump({
+                "config": self.config.doc,
+                "fsobjects": self.fsobjects.doc
+            }, f)
+
+
+def is_same_file(current: FileProps, hoard: HoardFileProps):
+    if current.size != hoard.size:
+        return False  # files differ by size
+
+    if abs(current.mtime - hoard.mtime) > 1e-3:
+        return False  # files differ by mtime
+
+    return True  # files are the same TODO implement hashing
 
 
 class RepoCommand:
@@ -85,7 +219,8 @@ class RepoCommand:
         current_uuid = load_current_uuid(self.repo)
         logging.info(f"Refreshing uuid {current_uuid}")
 
-        config = {"updated": datetime.now().isoformat()}
+        contents = Contents(os.path.join(hoard_folder(self.repo), f"{current_uuid}.contents"), contents_doc={})
+        contents.config.touch_updated()
 
         logging.info("Counting files to add")
         nfiles, nfolders = 0, 0
@@ -96,38 +231,23 @@ class RepoCommand:
                 bar(len(filenames) + len(dirnames))
 
         logging.info(f"Reading all files in {self.repo}")
-        fsobjects = {}
         with alive_bar(nfiles + nfolders) as bar:
             for dirpath, dirnames, filenames in walk_repo(self.repo):
                 for filename in filenames:
                     fullpath = str(os.path.join(dirpath, filename))
-                    file_props = {
-                        "size": os.path.getsize(fullpath),
-                        "mtime": os.path.getmtime(fullpath),
-                        "isdir": False}
-                    fsobjects[fullpath] = file_props
+                    contents.fsobjects.add_file(
+                        fullpath, size=os.path.getsize(fullpath),
+                        mtime=os.path.getmtime(fullpath))
                     bar()
                 for dirname in dirnames:
                     fullpath = str(os.path.join(dirpath, dirname))
-                    dir_props = {
-                        "isdir": True}
-                    fsobjects[fullpath] = dir_props
+                    contents.fsobjects.add_dir(fullpath)
                     bar()
 
         logging.info(f"Files read!")
 
-        doc = {
-            "config": config,
-            "fsobjects": fsobjects}
-
         logging.info(f"Writing cache...")
-        with open(os.path.join(hoard_folder(self.repo), "current.contents"), "w", encoding="utf-8") as f:
-            rtoml.dump(doc, f)
-
-        if not os.path.isfile(os.path.join(hoard_folder(self.repo), f"{current_uuid}.contents")):
-            shutil.copy(
-                os.path.join(hoard_folder(self.repo), "current.contents"),
-                os.path.join(hoard_folder(self.repo), f"{current_uuid}.contents"))
+        contents.write()
 
         logging.info(f"Refresh done!")
 
@@ -140,14 +260,14 @@ class RepoCommand:
         remote_uuid = self._resolve_remote_uuid(remote)
 
         logging.info(f"Reading repo {self.repo}...")
-        doc = read_contents_toml(self.repo, remote=remote_uuid)
+        contents = Contents.load(self._contents_filename(remote_uuid))
         logging.info(f"Read repo!")
 
         print(f"Result for [{remote}] with UUID {remote_uuid}.")
-        print(f"Last updated on {datetime.fromisoformat(doc["config"]["updated"])}.")
-        print(f"  # files = {len([f for f, data in doc['fsobjects'].items() if not data['isdir']])}"
-              f" of size {format_size(sum(data['size'] for f, data in doc['fsobjects'].items() if not data['isdir']))}")
-        print(f"  # dirs  = {len([f for f, data in doc['fsobjects'].items() if data['isdir']])}")
+        print(f"Last updated on {contents.config.updated}.")
+        print(f"  # files = {len(contents.fsobjects.files)}"
+              f" of size {sum(f.size for f in contents.fsobjects.files.values())}")
+        print(f"  # dirs  = {len(contents.fsobjects.dirs)}")
 
     def _resolve_remote_uuid(self, remote):
         remotes = self._remotes_names()
@@ -206,39 +326,42 @@ class RepoCommand:
             rtoml.dump(config_doc, f)
         logging.info(f"Config done!")
 
-    def _hoard_toml(self) -> Dict[str, Any]:
-        if not os.path.isfile(self._hoard_contents_filename()):
-            with open(self._hoard_contents_filename(), "w", encoding="utf-8") as f:
-                config = {"updated": datetime.now().isoformat()}
-                rtoml.dump({
-                    "config": config,
-                    "fsobjects": {},
-                }, f)
-        with open(self._hoard_contents_filename(), "r", encoding="utf-8") as f:
-            return rtoml.load(f)
-
     def _hoard_contents_filename(self):
         return os.path.join(hoard_folder(self.repo), HOARD_CONTENTS_FILENAME)
 
     def commit_local(self):
         logging.info(f"Loading hoard TOML...")
-        hoard_doc = self._hoard_toml()
+        hoard = HoardContents.load(self._hoard_contents_filename())
         logging.info(f"Loaded hoard TOML!")
 
-        current_doc = read_contents_toml(self.repo, remote="current")
         current_uuid = load_current_uuid(self.repo)
+        current_contents = Contents.load(self._contents_filename(current_uuid))
 
         logging.info("Merging local changes...")
-        hoard_fsobjects = hoard_doc["fsobjects"]
-        for current_fso, props in current_doc["fsobjects"].items():
-            if current_fso not in hoard_fsobjects.keys():
-                hoard_fsobjects[current_fso] = props
-                hoard_fsobjects[current_fso]["available"] = current_uuid
+        for curr_file, props in current_contents.fsobjects.files.items():
+            if curr_file not in hoard.fsobjects.files.keys():
+                logging.info(f"new file found: {curr_file}")
+                hoard.fsobjects.add_available_file(curr_file, props, current_uuid)
+            elif is_same_file(current_contents.fsobjects.files[curr_file], hoard.fsobjects.files[curr_file]):
+                pass  # logging.info(f"Skip adding {curr_file} as its contents are equal!")
+            else:
+                logging.info(f"updating existing file {curr_file}")
+
+                hoard.fsobjects.update_file(curr_file, props)
+
+        for curr_dir, props in current_contents.fsobjects.dirs.items():
+            if curr_dir not in hoard.fsobjects.dirs.keys():
+                logging.info(f"new dir found: {curr_dir}")
+                hoard.fsobjects.add_dir(curr_dir)
+            else:
+                pass  # dir is there already
 
         logging.info("Writing updated hoard contents...")
-        with open(self._hoard_contents_filename(), "w", encoding="utf-8") as f:
-            rtoml.dump(hoard_doc, f)
+        hoard.write()
         logging.info("Local commit DONE!")
+
+    def _contents_filename(self, remote_uuid):
+        return os.path.join(hoard_folder(self.repo), f"{remote_uuid}.contents")
 
 
 # Press the green button in the gutter to run the script.
