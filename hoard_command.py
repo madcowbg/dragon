dee8d006021ea818a47b747fce971d4964ff514e
Bono Nonchev <11063463+madcowbg@users.noreply.github.com>
2025-03-04 13:53:23 +0200

    added create and sync to hoard unit tests

--- hoard_command.py
+++ hoard_command.py
@@ -129,22 +129,24 @@ class HoardCommand(object):
         logging.info(f"Loaded hoard TOML!")
         logging.info(f"Computing status ...")
 
-        print(f"Status of {remote_uuid}:")
-        for curr_file, props in current_contents.fsobjects.files.items():
-            if curr_file not in hoard.fsobjects.files.keys():
-                print(f"A {curr_file}")
-            elif is_same_file(current_contents.fsobjects.files[curr_file], hoard.fsobjects.files[curr_file]):
-                pass  # logging.info(f"Skip adding {curr_file} as its contents are equal!")
-            else:
-                print(f"M {curr_file}")
+        with StringIO() as out:
+            out.write(f"Status of {remote_uuid}:\n")
+            for curr_file, props in current_contents.fsobjects.files.items():
+                if curr_file not in hoard.fsobjects.files.keys():
+                    out.write(f"A {curr_file}\n")
+                elif is_same_file(current_contents.fsobjects.files[curr_file], hoard.fsobjects.files[curr_file]):
+                    pass  # logging.info(f"Skip adding {curr_file} as its contents are equal!")
+                else:
+                    out.write(f"M {curr_file}\n")
 
-        for curr_dir, props in current_contents.fsobjects.dirs.items():
-            if curr_dir not in hoard.fsobjects.dirs.keys():
-                print(f"AD {curr_dir}")
-            else:
-                pass  # dir is there already
+            for curr_dir, props in current_contents.fsobjects.dirs.items():
+                if curr_dir not in hoard.fsobjects.dirs.keys():
+                    out.write(f"AD {curr_dir}\n")
+                else:
+                    pass  # dir is there already
 
-        logging.info("Computing status done!")
+            logging.info("Computing status done!")
+            return out.getvalue()
 
     def mount_remote(self, remote: str, mount_point: str, force: bool = False):
         remote_uuid = self._resolve_remote_uuid(remote)
@@ -183,7 +185,7 @@ class HoardCommand(object):
         with StringIO() as out:
             out.write(f"{len(config.remotes)} total remotes.\n")
             for remote in config.remotes.all():
-                name_prefix = f"[{remote.name} " if remote.name != "INVALID" else ""
+                name_prefix = f"[{remote.name}] " if remote.name != "INVALID" else ""
 
                 out.write(f"  {name_prefix}{remote.uuid}\n")
             return out.getvalue()
--- test_hoard_command.py
+++ test_hoard_command.py
@@ -7,10 +7,16 @@ from main import TotalCommand
 from test_repo_command import populate
 
 
+def populate_hoard(tmpdir: str):
+    populate(tmpdir)
+
+    os.mkdir(join(tmpdir, "hoard"))
+
+
 class TestRepoCommand(unittest.TestCase):
     def setUp(self):
         self.tmpdir = tempfile.TemporaryDirectory()
-        populate(self.tmpdir.name)
+        populate_hoard(self.tmpdir.name)
 
     def tearDown(self):
         self.tmpdir.cleanup()
@@ -20,9 +26,29 @@ class TestRepoCommand(unittest.TestCase):
         cave_cmd.init()
         cave_cmd.refresh()
 
-        os.mkdir(join(self.tmpdir.name, "hoard"))
+        repo_uuid = cave_cmd.current_uuid()
+
         hoard_cmd = TotalCommand(path=join(self.tmpdir.name, "hoard")).hoard
         res = hoard_cmd.remotes()
 
         self.assertEqual("0 total remotes.", res.strip())
+
+        hoard_cmd.add_remote(remote_path=join(self.tmpdir.name, "repo"), name="repo-in-local")
+        res = hoard_cmd.remotes()
+        self.assertEqual(f"1 total remotes.\n  [repo-in-local] {repo_uuid}", res.strip())
+
+    def test_sync_to_hoard(self):
+        cave_cmd = TotalCommand(path=join(self.tmpdir.name, "repo")).cave
+        cave_cmd.init()
+        cave_cmd.refresh()
+
+        hoard_cmd = TotalCommand(path=join(self.tmpdir.name, "hoard")).hoard
+        hoard_cmd.add_remote(remote_path=join(self.tmpdir.name, "repo"), name="repo-in-local")
+
+        repo_uuid = cave_cmd.current_uuid()
+
+        res = hoard_cmd.status("repo-in-local")
+        self.assertEqual(
+            f"Status of {repo_uuid}:\nA wat/test.me.different\nA wat/test.me.once\nA wat/test.me.twice\nAD wat",
+            res.strip())
 
