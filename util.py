8e7b3c63818fa0a7169c08b874c7eb67bc2c587b
Bono Nonchev <11063463+madcowbg@users.noreply.github.com>
2025-03-18 16:29:22 -0400

    added an experiment to create git repo representing sqlite data

--- util.py
+++ util.py
@@ -1,7 +1,9 @@
 import asyncio
+import threading
 from asyncio import Queue, TaskGroup
 from sqlite3 import Cursor, Row
 from typing import List, Tuple, Any, Callable, Coroutine, Dict, TypeVar
+import queue
 
 
 def format_size(size: int) -> str:
@@ -28,17 +30,17 @@ R = TypeVar('R')
 
 def run_async_in_parallel(
         args: List[Tuple[Any, ...]], fun: Callable[[Any, ...], Coroutine[Any, Any, R]], ntasks: int = 10) -> List[R]:
-    queue: Queue[Tuple[int, Any]] = asyncio.Queue()
+    q: Queue[Tuple[int, Any]] = asyncio.Queue()
     for i, item in enumerate(args):
-        queue.put_nowait((i, item))
+        q.put_nowait((i, item))
 
     result_per_invocation: Dict[int, Any] = dict()
 
     async def process_item():
-        while not queue.empty():
-            idx, input_tuple = await queue.get()
+        while not q.empty():
+            idx, input_tuple = await q.get()
             result = await fun(*input_tuple)
-            queue.task_done()
+            q.task_done()
 
             assert idx not in result_per_invocation
             result_per_invocation[idx] = result
@@ -48,9 +50,34 @@ def run_async_in_parallel(
             for _ in range(ntasks):
                 tg.create_task(process_item())
 
-            await queue.join()
+            await q.join()
 
     asyncio.run(run_all())
+
+    return [v for i, v in sorted(result_per_invocation.items())]
+
+
+def run_in_parallel_threads(
+        args: List[Tuple[Any, ...]], fun: Callable[[Any, ...], R], ntasks: int = 10) -> List[R]:
+    q: queue.SimpleQueue[Tuple[int, Any]] = queue.SimpleQueue()
+    for i, item in enumerate(args):
+        q.put_nowait((i, item))
+
+    result_per_invocation: Dict[int, Any] = dict()
+
+    def process_item():
+        while not q.empty():
+            idx, input_tuple = q.get_nowait()
+            result = fun(*input_tuple)
+
+            assert idx not in result_per_invocation
+            result_per_invocation[idx] = result
+
+    threads = [threading.Thread(target=process_item) for _ in range(ntasks)]
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
 
     return [v for i, v in sorted(result_per_invocation.items())]
 
