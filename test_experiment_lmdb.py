7e62ab2f184116025f178898d13df0982a64f81a
Bono Nonchev <11063463+madcowbg@users.noreply.github.com>
2025-04-18 19:40:31 -0400

    introduced data classes for better typing

--- test_experiment_lmdb.py
+++ test_experiment_lmdb.py
@@ -3,12 +3,13 @@ import enum
 import hashlib
 import logging
 import unittest
-from typing import List, Tuple, Dict
+from typing import List, Tuple, Dict, Iterable
 
 import lmdb
 import msgpack
 from alive_progress import alive_it, alive_bar
 from lmdb import Transaction
+from propcache import cached_property
 
 from contents.hoard_props import HoardFileStatus
 from sql_util import sqlite3_standard
@@ -28,6 +29,89 @@ class ObjectType(enum.Enum):
 #
 # blob is tree:
 #  Type.TREE, Dict[obj name to object_id]
+
+type ObjectID = bytes
+
+
+@dataclasses.dataclass
+class TreeObject:
+    children: Dict[str, ObjectID]
+
+    @cached_property
+    def serialized(self) -> bytes:
+        return msgpack.packb((ObjectType.TREE.value, self.children))
+
+    @staticmethod
+    def load(data: bytes) -> "TreeObject":
+        object_type, children = msgpack.unpackb(data)
+        assert object_type == ObjectType.TREE.value
+        return TreeObject(children=children)
+
+
+@dataclasses.dataclass
+class FileObject:
+    fasthash: str
+    size: int
+
+    @cached_property
+    def serialized(self) -> bytes:
+        return msgpack.packb((ObjectType.FILE.value, self.fasthash, self.size))
+
+    @staticmethod
+    def load(data: bytes) -> "FileObject":
+        object_type, fasthash, size = msgpack.unpackb(data)
+        assert object_type == ObjectType.FILE.value
+        return FileObject(fasthash=fasthash, size=size)
+
+
+def load_tree_or_file(obj_id: bytes, txn: Transaction) -> FileObject | TreeObject:
+    obj_packed = txn.get(obj_id)  # todo use streaming op
+    obj_data = msgpack.loads(obj_packed)  # fixme make this faster by extracting type away
+    if obj_data[0] == ObjectType.FILE.value:
+        return FileObject.load(obj_packed)
+    elif obj_data[0] == ObjectType.TREE.value:
+        return TreeObject.load(obj_packed)
+    else:
+        raise ValueError(f"Unrecognized type {obj_data[0]}")
+
+
+class ExpandableTreeObject:
+    def __init__(self, data: TreeObject, txn: Transaction):
+        self.txn = txn
+        self._children = data.children
+
+        self._files: Dict[str, FileObject] | None = None
+        self._dirs: Dict[str, ExpandableTreeObject] | None = None
+
+    @property
+    def files(self) -> Dict[str, FileObject]:
+        if self._files is None:
+            self._load()
+        return self._files
+
+    @property
+    def dirs(self) -> Dict[str, "ExpandableTreeObject"]:
+        if self._dirs is None:
+            self._load()
+        return self._dirs
+
+    def _load(self):
+        self._files = dict()
+        self._dirs = dict()
+
+        for name, obj_id in self._children.items():
+            obj = load_tree_or_file(obj_id, self.txn)
+            if isinstance(obj, FileObject):
+                self._files[name] = obj
+            elif isinstance(obj, TreeObject):
+                self._dirs[name] = ExpandableTreeObject(obj, self.txn)
+            else:
+                raise TypeError(f"Unexpected type {type(obj)}")
+
+    @staticmethod
+    def create(obj_id: bytes, txn: Transaction) -> "ExpandableTreeObject":
+        return ExpandableTreeObject(TreeObject.load(txn.get(obj_id)), txn)
+
 
 # @unittest.skip("Made to run only locally to benchmark")
 class MyTestCase(unittest.TestCase):
@@ -71,13 +155,6 @@ class MyTestCase(unittest.TestCase):
                 with env.begin(db=env.open_db("repos".encode()), write=True) as txn:
                     txn.put(uuid.encode(), uuid_root_id)
 
-            # for fullpath, fasthash, size in all_data:
-            #     path = fullpath.split("/")
-            #
-            #     root_id = up_to_date_object(txn, root_id, path, (fasthash, size))
-            #
-            #     txn.put("HEAD".encode(), root_id)
-
     def _list_uuids(self, conn) -> List[str]:
         curr = conn.cursor()
         curr.row_factory = FIRST_VALUE
@@ -87,12 +164,19 @@ class MyTestCase(unittest.TestCase):
     def test_fully_load_lmdb(self):
         env = lmdb.open("test/example.lmdb", max_dbs=5)  # , map_size=(1 << 30) // 4)
 
-        with alive_bar(title="loading from lmdb...") as bar:
-            with env.begin(db=env.open_db("repos".encode()), write=False) as txn:
-                root_id = txn.get("HEAD".encode())
+        with env.begin(db=env.open_db("repos".encode()), write=False) as txn:
+            root_id = txn.get("HEAD".encode())
 
-            with env.begin(db=env.open_db("objects".encode()), write=False) as txn:
-                root = load_recursive(txn, root_id, bar)
+        with env.begin(db=env.open_db("objects".encode()), write=False) as txn:
+            root = ExpandableTreeObject.create(root_id, txn)
+
+            def all_files(tree: ExpandableTreeObject) -> Iterable[FileObject]:
+                yield from tree.files.values()
+                for subtree in tree.dirs.values():
+                    yield from all_files(subtree)
+
+            all_files = list(alive_it(all_files(root), title="loading from lmdb..."))
+            logging.warning(f"# all_files: {len(all_files)}")
 
     # @unittest.skip("Made to run only locally to benchmark")
     def test_dump_lmdb(self):
@@ -104,25 +188,10 @@ class MyTestCase(unittest.TestCase):
                     msgpack.dump(list(((k, v) for k, v in curr)), f)
 
 
-def load_recursive(txn, obj_id: bytes, bar) -> Dict[str, any] | Tuple[any]:
-    bar()
-    obj_packed = txn.get(obj_id)
-    obj_data = msgpack.loads(obj_packed)
-    if obj_data[0] == ObjectType.FILE.value:
-        return obj_data
-    elif obj_data[0] == ObjectType.TREE.value:
-        res = dict()
-        for child_name, child_id in obj_data[1].items():
-            res[child_name] = load_recursive(txn, child_id, bar)
-        return res
-    else:
-        raise ValueError(f"Unrecognized type {obj_data[0]}")
-
-
-def add_all(all_data, txn: Transaction) -> bytes:
+def add_all(all_data: Tuple[str, str, int], txn: Transaction) -> bytes:
     # every element is a partially-constructed object
-    # (name, {sub: sub_id})
-    stack: List[Tuple[str | None, Dict[str, bytes]]] = [("", dict())]
+    # (name, partial TreeObject)
+    stack: List[Tuple[str | None, TreeObject]] = [("", TreeObject(dict()))]
     for fullpath, fasthash, size in alive_it(all_data, title="adding all data..."):
         pop_and_write_nonparents(txn, stack, fullpath)
 
@@ -136,7 +205,7 @@ def add_all(all_data, txn: Transaction) -> bytes:
         rel_path = fullpath[len(top_obj_path) + 1:-len(file_name)].split("/")
         for path_elem in rel_path[:-1]:
             current_path += "/" + path_elem
-            stack.append((current_path, dict()))
+            stack.append((current_path, TreeObject(dict())))
 
         # add file to current's children
         file_data = (ObjectType.FILE.value, fasthash, size)
@@ -145,9 +214,9 @@ def add_all(all_data, txn: Transaction) -> bytes:
 
         txn.put(file_id, file_packed)
 
-        top_obj_path, children = stack[-1]
+        top_obj_path, tree_obj = stack[-1]
         assert is_child_of(fullpath, top_obj_path) and fullpath[len(top_obj_path) + 1:].find("/") == -1
-        children[file_name] = file_id
+        tree_obj.children[file_name] = file_id
 
     pop_and_write_nonparents(txn, stack, "/")  # commits the stack
     assert len(stack) == 1
@@ -156,60 +225,29 @@ def add_all(all_data, txn: Transaction) -> bytes:
     return obj_id
 
 
-def pop_and_write_nonparents(txn, stack, fullpath):
+def pop_and_write_nonparents(txn, stack: List[Tuple[str | None, TreeObject]], fullpath: str):
     while not is_child_of(fullpath, stack[-1][0]):  # this is not a common ancestor
         child_id, child_path = pop_and_write_obj(stack, txn)
 
         # add to parent
-        _, parent_children = stack[-1]
+        _, parent_obj = stack[-1]
         child_name = child_path[child_path.rfind("/") + 1:]
-        parent_children[child_name] = child_id
+        parent_obj.children[child_name] = child_id
 
 
 def is_child_of(fullpath, parent) -> bool:
     return fullpath.startswith(parent) and fullpath[len(parent)] == "/"
 
 
-def pop_and_write_obj(stack, txn):
-    top_obj_path, children = stack.pop()
+def pop_and_write_obj(stack: List[Tuple[str | None, TreeObject]], txn):
+    top_obj_path, tree_obj = stack.pop()
 
     # store currently constructed object in tree
-    obj_data = (ObjectType.TREE.value, children)
-    obj_packed = msgpack.packb(obj_data)
+    obj_packed = tree_obj.serialized
     obj_id = hashlib.sha1(obj_packed).digest()
     txn.put(obj_id, obj_packed)
 
     return obj_id, top_obj_path
-
-
-def up_to_date_object(txn: Transaction, current_id: bytes | None, path: List[str], file: any) -> bytes:
-    current_object_s = txn.get(current_id) if current_id is not None else None
-
-    if len(path) == 0:  # is current file
-        new_obj_s = msgpack.dumps((ObjectType.FILE.value, file))
-        if new_obj_s == current_object_s:
-            return current_id
-        else:  # a new/modified file
-            new_id = hashlib.md5(new_obj_s).digest()
-            if not txn.get(new_id):
-                txn.put(new_id, new_obj_s)
-            return new_id
-    else:
-        current_object = msgpack.loads(current_object_s) if current_object_s is not None else (ObjectType.TREE.value, dict())
-        current_child_name = path[0]
-
-        current_child_id = current_object[1].get(current_child_name, None)
-
-        new_child_id = up_to_date_object(txn, current_child_id, path[1:], file)
-        current_object[1][current_child_name] = new_child_id
-
-        # store the new object
-        new_obj_data = msgpack.dumps(current_object)
-        new_obj_id = hashlib.md5(new_obj_data).digest()
-
-        if not txn.get(new_obj_id):
-            txn.put(new_obj_id, new_obj_data)
-        return new_obj_id
 
 
 if __name__ == '__main__':
