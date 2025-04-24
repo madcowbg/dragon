f5e1be4e04b5efecc3dce6956d15eebfd017f655
Bono Nonchev <bono.nonchev@gmail.com>
2025-04-24 16:22:36 -0400

    extended pull cases
    found the bad case when using backup repos - they overwrite changes.

--- lmdb_storage/test_workflows.py
+++ lmdb_storage/test_workflows.py
@@ -8,7 +8,7 @@ from lmdb_storage.object_store import ObjectStorage
 from lmdb_storage.test_experiment_lmdb import dump_tree
 from lmdb_storage.test_merge_trees import populate_trees
 from lmdb_storage.three_way_merge import ThreewayMerge
-from lmdb_storage.tree_structure import ObjectID
+from lmdb_storage.tree_structure import ObjectID, remove_file_object
 
 
 def pull_contents(env: ObjectStorage, repo_uuid: str, staging_id: ObjectID, fetch_new: Set[str]):
@@ -94,12 +94,11 @@ class TestWorkflows(unittest.TestCase):
 
         pull_contents(env, repo_uuid="incoming-uuid", staging_id=incoming_id, fetch_new={"full-uuid"})
 
-        roots = env.roots(write=False)
         current_full_id = roots["full-uuid"].get_current()
         current_hoard_id = roots["HOARD"].get_current()
         current_incoming_id = roots["incoming-uuid"].get_current()
 
-        with env.objects(write=True) as objects:
+        with env.objects(write=False) as objects:
             self.assertEqual([
                 ('$ROOT', 1),
                 ('$ROOT/test.me.1', 2, 'e10d2982020fc21760e4e5245b57f664'),
@@ -132,3 +131,111 @@ class TestWorkflows(unittest.TestCase):
                 ('$ROOT/wat/test.me.6', 2, 'd6a296dae0ca6991df926b8d18f43cc5'),
                 ('$ROOT/wat/test.me.7', 2, '46e7da788d1c605a2293d580eeceeefd')],
                 dump_tree(objects, current_hoard_id, show_fasthash=True))
+
+        # remove a file from staging
+        with env.objects(write=True) as objects:
+            staging_incoming_id = remove_file_object(objects, current_incoming_id, "wat/test.me.6".split("/"))
+            self.assertNotEqual(incoming_id, staging_incoming_id)
+
+        pull_contents(env, repo_uuid="incoming-uuid", staging_id=staging_incoming_id, fetch_new={"full-uuid"})
+
+        current_full_id = roots["full-uuid"].get_current()
+        current_hoard_id = roots["HOARD"].get_current()
+        current_incoming_id = roots["incoming-uuid"].get_current()
+        current_backup_id = roots["backup-uuid"].get_current()
+
+        self.assertEqual(current_incoming_id, staging_incoming_id)
+
+        with env.objects(write=False) as objects:
+            self.assertEqual([
+                ('$ROOT', 1),
+                ('$ROOT/test.me.1', 2, 'e10d2982020fc21760e4e5245b57f664'),
+                ('$ROOT/test.me.4', 2, '5c8ffab0d25ab7692378bf41d495e046'),
+                ('$ROOT/test.me.5', 2, '79e651dd08483b1483fb6e992c928e21'),
+                ('$ROOT/wat', 1),
+                ('$ROOT/wat/test.me.2', 2, '663c6e6ae648bb1a1a893b5134dbdd7b'),
+                ('$ROOT/wat/test.me.3', 2, 'ad78b7d31e769862e45f8efc7d39618d'),
+                ('$ROOT/wat/test.me.7', 2, '46e7da788d1c605a2293d580eeceeefd')],
+                dump_tree(objects, current_full_id, show_fasthash=True))
+
+            self.assertEqual([
+                ('$ROOT', 1),
+                ('$ROOT/test.me.4', 2, '5c8ffab0d25ab7692378bf41d495e046'),
+                ('$ROOT/test.me.5', 2, '79e651dd08483b1483fb6e992c928e21'),
+                ('$ROOT/wat', 1),
+                ('$ROOT/wat/test.me.3', 2, 'ad78b7d31e769862e45f8efc7d39618d')],
+                dump_tree(objects, current_incoming_id, show_fasthash=True))
+
+            self.assertEqual([
+                ('$ROOT', 1),
+                ('$ROOT/test.me.1', 2, 'e10d2982020fc21760e4e5245b57f664'),
+                ('$ROOT/test.me.4', 2, '5c8ffab0d25ab7692378bf41d495e046'),
+                ('$ROOT/test.me.5', 2, '79e651dd08483b1483fb6e992c928e21'),
+                ('$ROOT/wat', 1),
+                ('$ROOT/wat/test.me.2', 2, '663c6e6ae648bb1a1a893b5134dbdd7b'),
+                ('$ROOT/wat/test.me.3', 2, 'ad78b7d31e769862e45f8efc7d39618d'),
+                ('$ROOT/wat/test.me.7', 2, '46e7da788d1c605a2293d580eeceeefd')],
+                dump_tree(objects, current_hoard_id, show_fasthash=True))
+
+            self.assertEqual([
+                ('$ROOT', 1), ],
+                dump_tree(objects, current_backup_id, show_fasthash=True))
+
+        # adding the backup too
+        pull_contents(env, repo_uuid="backup-uuid", staging_id=backup_id, fetch_new={"full-uuid"})
+
+        current_full_id = roots["full-uuid"].get_current()
+        current_hoard_id = roots["HOARD"].get_current()
+        current_incoming_id = roots["incoming-uuid"].get_current()
+        current_backup_id = roots["backup-uuid"].get_current()
+
+        with env.objects(write=False) as objects:
+            self.assertEqual(current_backup_id, backup_id)
+
+            self.assertEqual([
+                ('$ROOT', 1),
+                ('$ROOT/test.me.4', 2, '5c8ffab0d25ab7692378bf41d495e046'),
+                ('$ROOT/test.me.5', 2, '79e651dd08483b1483fb6e992c928e21'),
+                ('$ROOT/wat', 1),
+                ('$ROOT/wat/test.me.3', 2, '2cbc8608c915e94723752d4f0c54302f')],  # fixme that is the bad place
+                dump_tree(objects, current_incoming_id, show_fasthash=True))
+
+            # fixme that should not have happened when pulling backups, should implement the logic
+            self.assertNotEqual(current_incoming_id, staging_incoming_id)  # one file was updated, using the backups
+
+            self.assertEqual([
+                ('$ROOT', 1),
+                ('$ROOT/test.me.1', 2, 'e10d2982020fc21760e4e5245b57f664'),
+                ('$ROOT/test.me.4', 2, '5c8ffab0d25ab7692378bf41d495e046'),
+                ('$ROOT/test.me.5', 2, '79e651dd08483b1483fb6e992c928e21'),
+                ('$ROOT/wat', 1),
+                ('$ROOT/wat/test.me.2', 2, '663c6e6ae648bb1a1a893b5134dbdd7b'),
+                ('$ROOT/wat/test.me.3', 2, '2cbc8608c915e94723752d4f0c54302f'),  # fixme that is the bad place
+                ('$ROOT/wat/test.me.7', 2, '46e7da788d1c605a2293d580eeceeefd')],
+                dump_tree(objects, current_full_id, show_fasthash=True))
+
+            self.assertEqual([
+                ('$ROOT', 1),
+                ('$ROOT/test.me.4', 2, '5c8ffab0d25ab7692378bf41d495e046'),
+                ('$ROOT/test.me.5', 2, '79e651dd08483b1483fb6e992c928e21'),
+                ('$ROOT/wat', 1),
+                ('$ROOT/wat/test.me.3', 2, '2cbc8608c915e94723752d4f0c54302f')],  # fixme that is the bad place
+                dump_tree(objects, current_incoming_id, show_fasthash=True))
+
+            self.assertEqual([
+                ('$ROOT', 1),
+                ('$ROOT/test.me.1', 2, 'e10d2982020fc21760e4e5245b57f664'),
+                ('$ROOT/test.me.4', 2, '5c8ffab0d25ab7692378bf41d495e046'),
+                ('$ROOT/test.me.5', 2, '79e651dd08483b1483fb6e992c928e21'),
+                ('$ROOT/wat', 1),
+                ('$ROOT/wat/test.me.2', 2, '663c6e6ae648bb1a1a893b5134dbdd7b'),
+                ('$ROOT/wat/test.me.3', 2, '2cbc8608c915e94723752d4f0c54302f'),  # fixme that is the bad place
+                ('$ROOT/wat/test.me.7', 2, '46e7da788d1c605a2293d580eeceeefd')],
+                dump_tree(objects, current_hoard_id, show_fasthash=True))
+
+            self.assertEqual([
+                ('$ROOT', 1),
+                ('$ROOT/test.me.1', 2, 'e10d2982020fc21760e4e5245b57f664'),
+                ('$ROOT/wat', 1),
+                ('$ROOT/wat/test.me.3', 2, '2cbc8608c915e94723752d4f0c54302f')], # fixme should not have changed!
+                dump_tree(objects, current_backup_id, show_fasthash=True))
